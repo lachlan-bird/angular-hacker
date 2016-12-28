273e90513b0a2268cafe021c323b98d682cd9567
lachlan-bird <lachlan.bird88@gmail.com>
2016-12-28 20:42:12 +1100

    Added Hacker News API Service. Modified stories and item components to observe from this service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
+import { DomainPipe } from './domain.pipe';
+
+// Providers
+import { HackerNewsAPIService } from './hackernews-api.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent,
+    DomainPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HackerNewsAPIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
